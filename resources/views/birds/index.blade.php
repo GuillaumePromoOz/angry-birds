2a84988e79c62ab5336146bf81b8fc382772cb5c
Guillaume Charneau <guillaume.promo.oz@gmail.com>
2021-09-20 11:14:08 +0200

    bird image dynamic

--- resources/views/birds/index.blade.php
+++ resources/views/birds/index.blade.php
@@ -8,21 +8,22 @@
 </div>
 
 
-<main class="container d-flex justify-content-center">
+<main class="container">
     <div class="row">
-        <div class="col">
+        @foreach ($birds as $bird)
+        <div class="col-6 col-sm-6 col-md-4 col-lg-3">
             <!-- Bird Card -->
-            @foreach ($birds as $bird)
             <div class="card border-secondary mb-3 bg-transparent" style="max-width: 20rem;">
                 <div class="card-header">{{ $bird->name }}</div>
+                <img class="home-product-image" src="{{ asset('images/' . $bird->image) }}" alt="product-image" />
                 <div class="card-body">
                     <h4 class="card-title">{{ $bird->image }}</h4>
                     <p class="card-text">{{ $bird->description }}</p>
                     <p class="card-text">{{ $bird->price }}</p>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 </main>
 @endsection
